4135b3370759ea660b2c4ba06b5a047a0c49fb30
Alanolog <alanrutyna2003@gmail.com>
2022-12-15 10:56:38 +0100

    signUp

--- components/molecules/SignUpForm/SignUpForm.tsx
+++ components/molecules/SignUpForm/SignUpForm.tsx
@@ -1,25 +1,37 @@
 import React from "react";
 import { StyledInput } from "../../index";
 import S from "./SignUpForm.module.scss";
+import { useAppDispatch } from "../../../redux/hooks";
+import { registerAction } from "../../../redux";
 
 export const SignUpForm: React.FC = () => {
+  const dispatch = useAppDispatch();
+
   const [username, setUsername] = React.useState("");
   const [email, setEmail] = React.useState("");
   const [password, setPassword] = React.useState("");
   return (
     <form className={S.signUpForm}>
-      <StyledInput
-        name="Nazwa użytkownika"
-        value={username}
-        setValue={setUsername}
-      />
+      <StyledInput name="Username" value={username} setValue={setUsername} />
       <StyledInput name="Email" value={email} setValue={setEmail} />
       <StyledInput
-        name="Hasło"
+        name="Password"
         value={password}
         setValue={setPassword}
         type="password"
       />
+      <button
+        className="authButton"
+        onClick={(e) => {
+          e.preventDefault();
+          dispatch(
+            //@ts-ignore redux thunk dont work with ts properly
+            registerAction(username, email, password)
+          );
+        }}
+      >
+        Sign Up
+      </button>
     </form>
   );
 };
