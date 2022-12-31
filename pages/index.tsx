a69e241fa6a1c8c3f59ba81b38cbeaeae6088667
Alanolog <alanrutyna2003@gmail.com>
2022-12-31 17:49:37 +0100

    request on load to run server

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,13 +2,21 @@ import React from "react";
 import { LoginForm, SignUpForm } from "../components";
 import { useRouter } from "next/router";
 import { useAppSelector } from "../redux/hooks";
+import { useDispatch } from "react-redux";
+import { fetchTasks } from "../redux";
+import { ThunkDispatch } from "redux-thunk";
+import { AnyAction } from "redux";
 
 export default function Home() {
   const router = useRouter();
+  const dispatch: ThunkDispatch<any, never, AnyAction> = useDispatch();
   const requestToken = useAppSelector((store) => store.user.userData?.token);
   if (requestToken?.length > 1) {
     router.push("/app");
   }
+  React.useEffect(() => {
+    dispatch(fetchTasks());
+  }, []);
   React.useEffect(() => {}, [requestToken]);
 
   return (
