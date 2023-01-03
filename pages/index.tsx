fe1e8a9d7acd901b15634ff14e949f23ce46abaf
Alanolog <alanrutyna2003@gmail.com>
2023-01-03 17:44:35 +0100

    do not show loading info when server is running

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,60 +1,77 @@
 import React from "react";
 import { LoginForm, SignUpForm } from "../components";
 import { useRouter } from "next/router";
-import { useAppSelector } from "../redux/hooks";
-import { useDispatch } from "react-redux";
 import { fetchTasks } from "../redux";
 import { ThunkDispatch } from "redux-thunk";
 import { AnyAction } from "redux";
+import { connect, ConnectedProps } from "react-redux";
+import { store } from "../models";
 
-export default function Home() {
+const mapStateToProps = (store: store) => ({
+  requestToken: store.user.userData?.token,
+  isWaitingTasks: store.tasks.loading,
+});
+
+const mapDispatchToProps = (dispatch: ThunkDispatch<any, never, AnyAction>) => {
+  return {
+    fetchTasks: () => dispatch(fetchTasks()),
+  };
+};
+const connector = connect(mapStateToProps, mapDispatchToProps);
+type PropsFromRedux = ConnectedProps<typeof connector>;
+
+const Home: React.FC<PropsFromRedux> = ({
+  fetchTasks,
+  requestToken,
+  isWaitingTasks,
+}) => {
   const router = useRouter();
-  const dispatch: ThunkDispatch<any, never, AnyAction> = useDispatch();
-  const requestToken = useAppSelector((store) => store.user.userData?.token);
   const [loadingInfo, setLoadingInfo] = React.useState<null | String>(null);
   if (requestToken?.length > 1) {
     router.push("/app");
   }
   React.useEffect(() => {
-    dispatch(fetchTasks());
+    fetchTasks();
     setLoadingInfo(window?.localStorage?.getItem("loadingInfo"));
   }, []);
   React.useEffect(() => {}, [requestToken, loadingInfo]);
 
   return (
     <main className="authMain">
-      {loadingInfo !== "confirmed" && (
-        <div
-          style={{
-            position: "fixed",
-            maxWidth: "250px",
-            border: "1px solid black",
-            background: "white",
-            padding: "10px",
-            borderRadius: "10px",
-          }}
-        >
-          this may take some time to get up the server. when loading will be
-          done you can register/login :){" "}
-          <p
+      {loadingInfo !== "confirmed" ||
+        (isWaitingTasks && (
+          <div
             style={{
-              cursor: "pointer",
-              position: "absolute",
-              top: 0,
-              right: 0,
-              transform: "translate(-50%,-80%)",
-            }}
-            onClick={() => {
-              window.localStorage.setItem("loadingInfo", "confirmed");
-              setLoadingInfo("confirmed");
+              position: "fixed",
+              maxWidth: "250px",
+              border: "1px solid black",
+              background: "white",
+              padding: "10px",
+              borderRadius: "10px",
             }}
           >
-            X
-          </p>
-        </div>
-      )}
+            this may take some time to get up the server. when loading will be
+            done you can register/login :){" "}
+            <p
+              style={{
+                cursor: "pointer",
+                position: "absolute",
+                top: 0,
+                right: 0,
+                transform: "translate(-50%,-80%)",
+              }}
+              onClick={() => {
+                window.localStorage.setItem("loadingInfo", "confirmed");
+                setLoadingInfo("confirmed");
+              }}
+            >
+              X
+            </p>
+          </div>
+        ))}
       <SignUpForm />
       <LoginForm />
     </main>
   );
-}
+};
+export default connector(Home);
